afd95896b7434b80802afac4dc22c3d3fc64698a
Pablo Jadzinsky <pjadzinsky@gmail.com>
2015-06-24 15:43:46 -0700

    Small change to plot1, now accepts startT and endT

--- Marcel De Jeu/Code/mouse.m
+++ Marcel De Jeu/Code/mouse.m
@@ -10,6 +10,12 @@ classdef mouse<handle
     methods
         % Class constructor
         function obj = mouse(file_path)
+            if nargin ~= 1
+                msg = ['mouse constructor needs one required argument. ', ...
+                    'Pass the path to the mat file with eye position. ', ...
+                    'Something like /Users/jadz/Documents/Notebook/Matlab/Eye tracking Mice/Marcel De Jeu/Data/mouse1-HV.mat'];
+                error(msg);
+            end
             % each file holds a struct that is named according to the mouse
             % I don't care about the particular mouse here and want to
             % access the data generically as just 'mouse'
@@ -30,7 +36,20 @@ classdef mouse<handle
             
         end
         
-        function plot1(obj)
+        function plot1(obj, varargin)
+            % plot vert and hori in between the times given
+            
+            p  = inputParser;   % Create an instance of the inputParser class.
+            p.addParameter('startT', 0, @(x)x>=0);
+            p.addParameter('endT', obj.tax(end), @(x) x>=0 && x<=obj.tax(end));
+            parse(p, varargin{:})
+            startT = p.Results.startT;
+            endT = p.Results.endT;
+
+            % convert startT and endT to points in tax
+            i0 = find(obj.tax >= startT, 1);
+            i1 = find(obj.tax >= endT, 1)-1;
+            
             % just plot both vert and horizontal eye movements against time
             % in the same plot
             s_plot_name = [inputname(1), '_HV_vs_T']
@@ -44,7 +63,7 @@ classdef mouse<handle
             
             figure(h)
             title('Eye position');
-            plot(obj.tax, obj.vert, 'r', obj.tax, obj.hori, 'b');
+            plot(obj.tax(i0:i1), obj.vert(i0:i1), 'r', obj.tax(i0:i1), obj.hori(i0:i1), 'b');
             
             xlabel('Time (s)');
             legend('vert', 'hori');
